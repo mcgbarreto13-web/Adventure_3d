18607edfe51629664622a5a8fb506b1b7cf6df08
mcgbarreto13-web <mcgbarreto13@gmail.com>
2026-02-10 20:00:46 -0300

    statemachine

---
+++ Assets/Scripts/Ebac/StateMachine/StateBase.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ebac.StateMachine
+{
+    public class StateBase : MonoBehaviour
+{
+   public virtual void OnStateEnter()
+    {
+        Debug.Log("OnStateEnter");
+    }
+    public virtual void OnStateStay()
+    {
+        Debug.Log("OnStateStay");
+    }
+    public virtual void OnStateExit()
+    {
+        Debug.Log("OnStateExit");
+    }
+
+}
+
+
+}
